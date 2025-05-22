027980038b7e46aa1a931f886d829393fb8c9391
Mugabo-art <m.kbonheur12@gmail.com>
2025-05-23 00:03:06 +0300

    Add solution - CodeHub

---
+++ 8-kyu/count-of-positives-slash-sum-of-negatives/count-of-positives-slash-sum-of-negatives.js
@@ -0,0 +1,19 @@
+function countPositivesSumNegatives(input) {
+    if (!input || input.length === 0) {
+        return [];
+    }
+    
+    let countPositives = 0;
+    let sumNegatives = 0;
+    
+    for (const num of input) {
+        if (num > 0) {
+            countPositives++;
+        } else if (num < 0) {
+            sumNegatives += num;
+        }
+    }
+    
+    return [countPositives, sumNegatives];
+// your code here
+}
